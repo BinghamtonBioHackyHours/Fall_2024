deb3e920c53ea477f4611cffeb23bf2819b97555
Josh Knecht <jknecht1@binghamton.edu>
2024-08-30 11:38:00 -0400

    jk

---
+++ scripts/week_2/blank.R
@@ -0,0 +1,69 @@
+
+# first we need to read in the data
+
+
+
+
+# look at the structure
+
+
+# dimensions of the data?
+
+
+
+# so there are 480 observations of 12 variables
+
+# looks like there are 4 categorical variables we might be interested in, let's see how 
+# many observations are in each of them
+
+
+
+# since we are probably looking for an affect of generation and treatment, how many
+# are in each combination of generation and treatment
+
+
+
+
+
+# it looked like there were some columns 
+# that had NAs, let's see how many NAs are in each column
+
+
+
+# looks like there is a lot of missing data! 
+
+# let's take a look at the total biomass column
+
+
+
+
+# seems there is a big spike at 0, what does it look like if we remove those
+
+
+
+# we often think about size in terms of log size, let's log it!
+
+
+
+# we will assume if total biomass = 0 that the plant did not germinate, what is the
+# proportion of plants that germinated
+
+
+
+
+# not great, does it seem like there is a difference between the generations?
+# the treatments?
+
+
+
+# how many entries have root and vegetative masses but no floral mass?
+
+
+
+# what is the proportion of germinated seeds that grew flowers?
+
+
+
+# is the germination rate different between generations in each treatment
+
+
